416dd0eec93a46a24b0453b61025df6fe17dc1e2
Marcin <m1.krzesinski@gmail.com>
2019-05-06 21:22:23 +0200

    marckrz
    _main

--- HappyDog/HappyDog_main.cpp
+++ HappyDog/HappyDog_main.cpp
@@ -2,23 +2,45 @@
 #include <cmath>
 #include <SFML/Window.hpp>
 
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <fstream>
+
 using namespace sf;
+using namespace std;
 
 int main()
 {
 
 	
-	RenderWindow window(VideoMode(320, 480), "Tetris");
+	RenderWindow window(VideoMode(480, 322), "HappyDog");
+
+	Texture t;
+	t.loadFromFile("images/480x322.png");
+	if (!t.loadFromFile("images/480x322.png")) { cout << "error\n"; }
+
+	Sprite s;
+	s.setTexture(t);
+
+
+	Event e;
 
 	while (window.isOpen())
 	{
-		window.clear();
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::KeyPressed && e.key.code == Keyboard::Escape) { window.close(); }
+
+		}
+
+
 	}
+
+	window.clear(Color::Black);
+	window.draw(s);
+	window.display();
+
 
 	return 0;
 }
-
-
-
-//kom
-
