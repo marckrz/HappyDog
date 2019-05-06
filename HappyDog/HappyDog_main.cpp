3536f4e1586f02965a37f3bb979f02e7ded09826
Marcin <m1.krzesinski@gmail.com>
2019-05-06 19:55:48 +0200

    Initial commit

---
+++ HappyDog/HappyDog_main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <SFML/Window.hpp>
+
+using namespace sf;
+
+int main()
+{
+
+	
+	RenderWindow window(VideoMode(320, 480), "Tetris");
+
+	while (window.isOpen())
+	{
+		window.clear();
+	}
+
+	return 0;
+}
+
+
+
+//kom
+
---
+++ HappyDog/Headers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include"SFML\Graphics.hpp"
+#include"SFML\System.hpp"
+#include"SFML\Window.hpp"
+
+using namespace sf;
